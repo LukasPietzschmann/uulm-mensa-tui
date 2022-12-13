c935fca836a154ea20f8c9cddafa577ea2b1b20c
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2022-12-13 22:57:16 +0100

    Extended utils

--- src/utils.hpp
+++ src/utils.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <algorithm>
+#include <regex>
 #include <string>
 #include <type_traits>
 
@@ -13,6 +15,30 @@ using InstanceOf = typename std::enable_if_t<std::is_base_of_v<B, T>>;
 
 template <typename T, typename... Args>
 using ConstructableWith = typename std::enable_if_t<std::is_constructible_v<T, Args...>>;
+
+template <typename T>
+inline const T& max(const T& t1) {
+	return t1;
+}
+
+template <typename T, typename... Args, typename = All<T, Args...>>
+inline const T& max(const T& t1, const T& t2, const Args&... args) {
+	return std::max(t1, max(t2, args...));
+}
+
+template <typename T>
+inline const T& min(const T& t1) {
+	return t1;
+}
+
+template <typename T, typename... Args, typename = All<T, Args...>>
+inline const T& min(const T& t1, const T& t2, const Args&... args) {
+	return std::min(t1, min(t2, args...));
+}
+
+inline std::string replace_sub(const std::string& orig, const std::string& sub, const std::string& repl) {
+	return std::regex_replace(orig, std::regex(sub), repl);
+}
 
 struct Showable {
 	virtual ~Showable() = default;
