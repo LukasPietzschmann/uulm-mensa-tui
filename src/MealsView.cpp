31009520ad6d835c1b918a3dde026b644ecafae4
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2023-08-27 21:29:10 +0200

    Separated main and side dishes

--- src/MealsView.cpp
+++ src/MealsView.cpp
@@ -5,12 +5,42 @@
 #include "MealView.hpp"
 #include "MealsView.hpp"
 
+const std::set<std::string_view> MealsView::m_main_meal_categories{
+	"Topf und Pfanne", "Prima Klima", "Fleisch und Fisch", "Sattmacher"};
+
 MealsView::MealsView(const std::vector<Meal>& meals) {
-	m_meals.reserve(meals.size());
-	for(const Meal& meal : meals)
-		m_meals.push_back(MealView(meal).Render());
+	m_main_meals.reserve(4);
+	m_secondary_meals.reserve(std::max(((int)meals.size()) - 4, 1));
+	for(const Meal& meal : meals) {
+		const Element& new_element = MealView(meal).Render();
+		if(m_main_meal_categories.contains(meal.category))
+			m_main_meals.push_back(new_element);
+		else
+			m_secondary_meals.push_back(new_element);
+	}
+
+	m_layout = Container::Vertical({
+		Container::Horizontal({
+			Renderer([&] { return text("") | flex; }),
+			Toggle(&m_categories, &m_selected_tab),
+			Renderer([&] { return text("") | flex; }),
+		}),
+		Renderer([&] { return separator(); }),
+		Container::Tab(
+			{
+				Renderer([&] { return vflow(m_main_meals); }),
+				Renderer([&] { return vflow(m_secondary_meals); }),
+			},
+			&m_selected_tab),
+	});
 }
 
-Element MealsView::Render() { return vflow(m_meals); }
+Element MealsView::Render() { return m_layout->Render(); }
 
-bool MealsView::Focusable() const { return false; }
+bool MealsView::OnEvent(Event e) { return m_layout->OnEvent(e); }
+
+void MealsView::OnAnimation(animation::Params& params) { m_layout->OnAnimation(params); }
+
+Component MealsView::ActiveChild() { return m_layout->ActiveChild(); }
+
+bool MealsView::Focusable() const { return m_layout->Focusable(); }
--- src/MealsView.hpp
+++ src/MealsView.hpp
@@ -2,6 +2,8 @@
 
 #include <ftxui/component/component.hpp>
 #include <ftxui/component/component_base.hpp>
+#include <set>
+#include <string_view>
 #include <vector>
 
 #include "Meal.hpp"
@@ -13,8 +15,18 @@ public:
 	MealsView(const std::vector<Meal>& meals);
 
 	Element Render() override;
+	bool OnEvent(Event e) override;
+	void OnAnimation(animation::Params& params) override;
+	Component ActiveChild() override;
 	bool Focusable() const override;
 
 private:
-	std::vector<Element> m_meals;
+	static const std::set<std::string_view> m_main_meal_categories;
+
+	int m_selected_tab{0};
+	std::vector<std::string> m_categories{"Main courses", "Side dishes"};
+	Component m_layout;
+
+	std::vector<Element> m_main_meals;
+	std::vector<Element> m_secondary_meals;
 };
